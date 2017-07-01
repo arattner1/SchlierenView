69a28b0aaff8bf83b7a96144517be1855d6634d2
Alex Rattner <Alex.Rattner@gmail.com>
2017-07-01 16:50:41 -0400

    first release

--- SchlierenViewer.py
+++ SchlierenViewer.py
@@ -18,6 +18,8 @@ W_cam = 1376
 H_cam = 768
 c.resolution = (W_cam,H_cam)
 
+#Other camera settings
+c.brightness = 50
 
 #Try autocropping...
 #Get an image
@@ -27,37 +29,50 @@ c.capture(testframe, 'rgb')
 brightness = np.sum(testframe, axis=2)
 #Find X-cm
 colsum = np.sum(brightness, axis=0)
-colsumthresh = 15000
+colsumthresh = 1.5*np.average(colsum)
 colsum = (colsum>colsumthresh)*colsum
 #Find Y-cm
 rowsum = np.sum(brightness, axis=1)
-rowsumthresh = 25000
+rowsumthresh = 1.5*np.average(rowsum)
 rowsum = (rowsum>rowsumthresh)*rowsum
 #Find spot size
 xinds = np.arange(W_cam)
-X_CM_Spot = np.sum(xinds*colsum)/(np.sum(colsum)+1)
-W_Spot = 1.5*(np.where(colsum>0)[0][-1] - np.where(colsum>0)[0][0])
+X_CM_spot = np.sum(xinds*colsum)/(np.sum(colsum)+1)
+W_spot = 1.1*(np.where(colsum>0)[0][-1] - np.where(colsum>0)[0][0])
 yinds = np.arange(H_cam)
-Y_CM_Spot = np.sum(yinds*rowsum)/(np.sum(rowsum)+1)
-H_Spot = 1.5*(np.where(rowsum>0)[0][-1] - np.where(rowsum>0)[0][0])
-S_spot = max(W_Spot, H_spot)
+Y_CM_spot = np.sum(yinds*rowsum)/(np.sum(rowsum)+1)
+H_spot = 1.1*(np.where(rowsum>0)[0][-1] - np.where(rowsum>0)[0][0])
+S_spot = max(W_spot, H_spot)
+#S_spot = 200
 
 #Convert spot into camera units (0-1)
-x_cam_spot = (X_CM_Spot-S_Spot/2.0)/W_cam
-y_cam_spot = (Y_CM_Spot-S_Spot/2.0)/H_cam
-w_cam_spot = S_Spot/W_cam
-h_cam_spot = S_Spot/H_cam
+x_cam_spot = (X_CM_spot-1.79*S_spot/2.0)/W_cam
+y_cam_spot = (Y_CM_spot+S_spot/2.0)/H_cam
+w_cam_spot = 1.79*S_spot/(W_cam)
+h_cam_spot = S_spot/H_cam
 
 if (x_cam_spot + w_cam_spot/2.0) > 1.0:
 	x_cam_spot = 1.0 - w_cam_spot
 if (y_cam_spot + h_cam_spot/2.0) > 1.0:
 	y_cam_spot = 1.0 - h_cam_spot
 
+#Final camera settings
+c.sharpness = 50
+#c.contrast = 50
+c.brightness = 70
+c.saturation = 30
+c.video_stabilization = False
+#c.exposure_compensation = 10
+
 
 #Camera display
-c.zoom = (x_cam_spot,0.0, w_cam_spot,1.0)  
+c.zoom = (x_cam_spot,y_cam_spot, w_cam_spot,h_cam_spot)  
 c.start_preview()
-sleep(10)
+
+#sleep(10)
+
+#End routine
+input()
 c.stop_preview()
 c.close()
 
