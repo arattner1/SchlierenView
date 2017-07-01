9fcf5c28403510b516b59bf42cf8df261c76f0a2
Alex Rattner <Alex.Rattner@gmail.com>
2017-07-01 14:20:30 -0400

    Some progress

--- SchlierenViewer.py
+++ SchlierenViewer.py
@@ -3,16 +3,25 @@
 # Alex Rattner, 2017-07-01
 
 import picamera
-from PIL import Image
 from time import sleep
+import numpy as np
 
+
+#Initialize camera
 c = picamera.PiCamera()
+wait(2)
 
-c.resolution = (1280, 720)
-c.framerate = 24
-c.start_preview()
+#Get camera info (resolution)
+resolution = c.resolution
+W_cam = resolution[0]
+H_cam = resolution[1]
 
-sleep(10)
+
+#Try autocropping...
+testframe = np.empty((W_cam, H_cam, 3), dtype=np.uint8)
+c.capture(testframe, 'rgb')
 
 
-c.stop_preview()
+# Wait indefinitely until the user terminates the script
+#while True:
+#    sleep(1)
